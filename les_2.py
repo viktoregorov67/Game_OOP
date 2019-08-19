a523f371cf3d03034a7c055979a3e8d6fb3cdc49
GefMar <30639918+GefMar@users.noreply.github.com>
2019-08-19 19:38:23 +0300

    Add les 2

--- les_2.py
+++ les_2.py
@@ -1,7 +1,7 @@
 """"Creator Sergey (Gefest) Romanchuk"""
 
 game_name = 'В поисках Байта'
-
+break_game = False
 # храним алфавит в виде строки
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
@@ -15,6 +15,15 @@ player = {'name': {'question': 'Как тебя зовут?', 'answer': None},
 
 print("добро пожаловать в игру", game_name)
 
+game_where_number = {
+    'question': 'Я задумал 16 чисел от 1 - 16 и расположил их в произвольном порядке в строке. Скажи мне Где какое?',
+    'numbers': ('1', '7', '8', '9', '3', '4', '13', '14', '5', '2', '6', '10', '11', '12', '15', '16'),
+    'str_pattern': '|',
+    'result_string': '|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|',
+    'game_statistic': 0,
+
+}
+
 """
 для того что-бы опросить нашего пользователя и в случае расширения опроса, 
 делаем цикл который пройдя по словарю будет задавать нужные вопросы  
@@ -25,14 +34,16 @@ for itm in player:
     он нужен для того что-бы снова задать вопрос если мы получим неверные данные
     '''
 
-    while True:
+    while not break_game:
         # тут мы задаем вопрос добавляя перенос строки, и сохраняем во временную переменную
         temp = input(player[itm]['question'] + '\n')
-
+        if temp.lower() == 'exit':
+            break_game = True
         # преобразуем возраст в int
         if itm == 'age':
             if temp.isdigit():
                 temp = int(temp)
+
             else:
                 print('Надо вводить число')
                 continue
@@ -60,7 +71,7 @@ for itm in player:
 
 # Проверяем возраст более 18, и основная логика игры у нас внутри
 
-if player['age']['answer'] > 18:
+if not break_game and player['age']['answer'] > 18:
     # данная переменая нужна что-бы учесть все ответы если пользователь очень старый для игры
     start_game = True
     if player['age']['answer'] >= 90:
@@ -70,12 +81,17 @@ if player['age']['answer'] > 18:
                          )
 
         for itm in temp_question:
+            if break_game:
+                break
             while start_game:
                 start_game = input(itm + '\n')
                 if start_game.lower() == 'да':
                     start_game = True
                 elif start_game.lower() == 'нет':
                     start_game = False
+                elif start_game.lower() == 'exit':
+                    break_game = True
+                    break
                 else:
                     print('Вы должны ответить да или нет')
                     continue
@@ -88,12 +104,55 @@ if player['age']['answer'] > 18:
     else:
         print('Привет', player['name']['answer'])
 
-    if start_game:
+    if not break_game and start_game:
         print('Я могу назвать буквы алфавита которых нет в твоем имени')
         for char in alphabet:
 
             if char not in player['name']['answer']:
                 print(char)
 
+        game_where_number = {
+            'question': 'Я задумал 16 чисел от 1 - 16 и расположил их в произвольном порядке в строке. '
+                        'Скажи мне Где какое?',
+            'numbers': ('1', '7', '8', '9', '3', '4', '13', '14', '5', '2', '6', '10', '11', '12', '15', '16',),
+            'for_numbers': ['4', '13', '14', '5', '1', '9', '7', '8', '3', '2', '15', '16', '6', '10', '11', '12', ],
+            'done_numbers': [],
+            'result_string': '|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|',
+            'game_step': 0,
+        }
+
+        while game_where_number['for_numbers']:
+            print(game_where_number['question'])
+            print(game_where_number['result_string'])
+            tmp_num = game_where_number["for_numbers"][-1]
+            current = game_where_number['numbers'].index(tmp_num)
+            answer = input(f'где число {tmp_num}?' + '\n')
+
+            if answer.lower() == 'exit':
+                break_game = True
+                break
+            elif answer not in game_where_number['numbers'] or not answer.isdigit():
+                print('некоректный ввод, повторите')
+                continue
+
+            if int(answer) - 1 == current:
+                print("Да действительно верно!")
+                tmp_result = game_where_number['result_string'].split('|')
+                tmp_result[int(answer)] = tmp_num
+                game_where_number['result_string'] = '|'.join(tmp_result)
+                game_where_number['done_numbers'].append(tmp_num)
+                game_where_number['for_numbers'].pop()
+            else:
+                print('нет не верно, попробуй еще.')
+                continue
+            game_where_number['game_step'] += 1
+        else:
+            if not break_game:
+                print('Поздравляю с победой!')
+                print(f'ВСЕГО ХОДОВ: {game_where_number["game_step"]}')
+
 else:
-    print('Тебе нельзя играть, слишком молодой')
+    if not break_game:
+        print('Тебе нельзя играть, слишком молодой')
+    else:
+        print("ВЫХОД ИЗ ИГРЫ")
